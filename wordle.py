f6bd0fe25ae8da3c46895c40c88800248b09d8fb
Hugo Franco <hugopereirafranco47@gmail.com>
2026-01-28 15:03:56 -0300

    importo random para usar en lista y seleccionar una palabra distinta por cada ronda

---
+++ wordle.py
@@ -0,0 +1,6 @@
+#Posibles palabras que pueden salir en wordle "importando random"
+import random
+
+posibles_palabras = ["autos","silla","mesas","manos"]
+
+palabra_secreta = random.choice(posibles_palabras)
