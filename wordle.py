e4fe8625723ecbf622746520fc6c23b28de806ba
Hugo Franco <hugopereirafranco47@gmail.com>
2026-01-28 15:07:56 -0300

    Creo un bucle While que se ejecutara hasta que intentos sea igual a 6, creo un input para que el usuario ingrese su palabra y agrego un acumulador que va sumando 1 a intentos

--- wordle.py
+++ wordle.py
@@ -4,3 +4,16 @@ import random
 posibles_palabras = ["autos","silla","mesas","manos"]
 
 palabra_secreta = random.choice(posibles_palabras)
+
+while intentos <= 6:
+
+    letras_verificadas = []
+    cantidad_letras = 5
+
+    #SE PIDE AL USUARIO INGRESAR UNA PALABRA
+
+    palabra_ingresada = input("Ingrese una palabra: ")
+
+    
+    
+    intentos += 1
