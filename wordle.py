f2e742d6f4c15d44b44084592d5add1f7b17a34e
Hugo Franco <hugopereirafranco47@gmail.com>
2026-01-28 15:14:57 -0300

    Creo una lista de letras que se iran verificando y un for donde se evaluara las condiciones que tienen que tener cada una de las palabras ingresadas, [] para las que se encuentran en la misma ubicacion que la letra de la palabra oculta y {} para las letras que tiene la palabra ingresada pero no se encuentran en la misma ubicacion que la de la palabra oculta

--- wordle.py
+++ wordle.py
@@ -7,6 +7,10 @@ palabra_secreta = random.choice(posibles_palabras)
 
 while intentos <= 6:
 
+    letras_verificadas = []
+
+    cantidad_letras = 5
+
     #SE PIDE AL USUARIO INGRESAR UNA PALABRA
 
     palabra_ingresada = input("Ingrese una palabra: ")
@@ -15,9 +19,19 @@ while intentos <= 6:
     while len(palabra_ingresada) != cantidad_letras:
         print("Intente ingresar una palabra de 5 caracteres... ")
         palabra_ingresada = input("Ingrese una palabra: ")
-    
 
+    #VERIFICAR LAS LETRAS DENTRO DE LA PALABRA
 
-    
+    for i in range(cantidad_letras):
+        las_palabras_son_iguales = palabra_ingresada[i] == palabra_secreta[i] # True o False
+        la_letra_existe_en_la_palabra = palabra_ingresada[i] in palabra_secreta 
+        
+        if las_palabras_son_iguales:
+              letras_verificadas.append(f"[{palabra_ingresada[i]}]")
+        elif la_letra_existe_en_la_palabra:
+                letras_verificadas.append(f"({palabra_ingresada[i]})")
+        else:
+                letras_verificadas.append(palabra_ingresada[i])
+
     
     intentos += 1
