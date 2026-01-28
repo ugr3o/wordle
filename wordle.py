5cd53809b099b2354a1374101bee05fe84f9d872
Hugo Franco <hugopereirafranco47@gmail.com>
2026-01-28 15:11:21 -0300

    agrego una variable que definira la cantidad de caracteres para las palabrasingresadas, ademas un while que evaluara que la palabra ingresada tengo 5 caracteres o sino no sale del bucle

--- wordle.py
+++ wordle.py
@@ -7,12 +7,16 @@ palabra_secreta = random.choice(posibles_palabras)
 
 while intentos <= 6:
 
-    letras_verificadas = []
-    cantidad_letras = 5
-
     #SE PIDE AL USUARIO INGRESAR UNA PALABRA
 
     palabra_ingresada = input("Ingrese una palabra: ")
+
+    #VERIFICAR QUE LA PALABRA INGRESADA TIENE 5 CARACTERES
+    while len(palabra_ingresada) != cantidad_letras:
+        print("Intente ingresar una palabra de 5 caracteres... ")
+        palabra_ingresada = input("Ingrese una palabra: ")
+    
+
 
     
     
